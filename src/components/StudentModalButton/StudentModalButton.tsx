13d024bf54d0b5ca7920e156560e5316a85e6205
debilla <dimuthu.billa@gmail.com>
2022-10-24 13:08:00 +0530

    S3 file uploading support added

--- src/components/StudentModalButton/StudentModalButton.tsx
+++ src/components/StudentModalButton/StudentModalButton.tsx
@@ -5,7 +5,7 @@ import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const REGION = "us-east-1";
-const s3Client = new S3Client({ region: REGION, credentials: {accessKeyId: 'AKIAS3A4WDWDV4XTXSUS', secretAccessKey: '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'} });
+const s3Client = new S3Client({ region: REGION, credentials: {accessKeyId: '', secretAccessKey: ''} });
 
 interface StudentModalButtonComponentProps {
     studentId: string,
