068f5961d9e6483f110cfce1d9ddd972ded73a9f
debilla <dimuthu.billa@gmail.com>
2023-03-28 09:08:58 +0530

    styling

--- src/containers/LandingContainer/LandingContainer.tsx
+++ src/containers/LandingContainer/LandingContainer.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TableComponent from '../../components/TableComponent/TableComponent';
 import { Navbar, Container, Nav, Button, Tab, Col, Row } from 'react-bootstrap';
 import { QueryClient, QueryClientProvider } from "react-query";
+import logo from '../../assets/images/logo1.png';
 
 const queryClient = new QueryClient();
 
@@ -16,15 +17,18 @@ export default class LandingContainer extends Component<LandingContainerProp> {
 
     render(): React.ReactNode {
         return <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" fixed="top">
                 <Container fluid>
-                    <Navbar.Brand>Serendib Foundation</Navbar.Brand>
+                    <Navbar.Brand style={{backgroundColor: 'black', borderRadius: '10px'}}><span style={{margin: '10px'}}>Serendib Foundation</span></Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="http://credoxyz.epizy.com/?i=1"><img style={{width: '50px', height: '50px', borderRadius: '50px'}} src={logo} /></Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
-            <Container style={btn} fluid>
+            <Container style={{...btn, top: '82px', position: 'relative'}} fluid>
                 <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                     <Row style={{backgroundColor: '#212121'}}>
-                        <Col sm={1} style={{backgroundColor: 'black'}}>
+                        <Col sm={1} style={{backgroundColor: 'black', height: '100%', position: 'fixed'}}>
                             <Nav style={{marginTop: '15px'}} variant="pills" className="flex-column">
                                 <Nav.Item>
                                     <Nav.Link eventKey="first">Student</Nav.Link>
@@ -34,7 +38,7 @@ export default class LandingContainer extends Component<LandingContainerProp> {
                                 </Nav.Item>
                             </Nav>
                         </Col>
-                        <Col sm={11}>
+                        <Col sm={11} style={{marginLeft: '155px', position: 'relative'}}>
                             <QueryClientProvider client={queryClient}>
                                 <Tab.Content>
                                     <Tab.Pane eventKey="first">
