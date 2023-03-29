5427e57d0633a6599579bb4479f281a40f79ed4c
debilla <dimuthu.billa@gmail.com>
2023-03-29 16:09:14 +0530

    Login component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import LandingContainer from "./containers/LandingContainer/LandingContainer";
+import LoginComponent from "./components/LoginComponent/LoginComponent";
+import { useState } from "react";
 
 export default function App() {
+  const [loginShow, setLoginShow] = useState(true);
+
+  const handleClose = () => {
+    setLoginShow(false);
+  }
+
   return (
-    <LandingContainer />
+    <><LoginComponent show={loginShow} handleClose={handleClose} />
+    <LandingContainer /></>
   );
 }
---
+++ src/components/LoginComponent/LoginComponent.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+
+interface LoginProps {
+  show: boolean;
+  handleClose: () => void;
+}
+
+const LoginComponent: React.FC<LoginProps> = ({ show, handleClose }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginFailed, setLoginFailed] = useState(false);
+
+  const handleLogin = (e: any) => {
+    if (email === "geetha" && password === "geetha12345") {
+      handleClose();
+    } else {
+      setLoginFailed(true);
+    }
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleLogin}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" type="submit" onClick={handleLogin}>
+          Login
+        </Button>
+        {loginFailed && <Form.Label>
+          <div
+            style={{
+              backgroundColor: "rgba(255, 100, 100, 0.85)",
+              padding: "10px",
+              borderRadius: "4px",
+            }}
+          >
+            Login failed. Please try again.
+          </div>
+        </Form.Label>}
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default LoginComponent;
